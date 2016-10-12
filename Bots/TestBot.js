09173710db1b9126aa15857d754a68784766eb03
FireGamer3 <gm31334@gmail.com>
2016-10-12 01:00:29 -0500

    fixed testbot

--- Bots/TestBot.js
+++ Bots/TestBot.js
@@ -4,18 +4,11 @@ var testBot = new DisnodeBot("./Bots/TestBotConfig.json"); //Defines the testBot
 
 var OnLoad = function(){
   //testBot.service.AddService("KikService", "KikService");
-<<<<<<< Updated upstream
   testBot.service.AddService("TwitchService", "TwitchService");
   testBot.service.AddService("DiscordService", "DiscordService")
-  //testBot.service.AddService("KikService", "KikService");
-    testBot.manager.AddManager("WeatherManager", "WeatherManager");
-  testBot.manager.AddManager("CustomCommands", "CustomCommands");
-=======
-  //testBot.service.AddService("TwitchService", "TwitchService");
-  testBot.service.AddService("DiscordService", "DiscordService");
+  testBot.manager.AddManager("WeatherManager", "WeatherManager");
 
 testBot.manager.AddManager("CustomCommands", "CustomCommands");
->>>>>>> Stashed changes
 testBot.manager.AddManager("CAHGame", "CAHGame");
 testBot.command.on("RawCommand_test",function(commandData){
       testBot.service.SendMessage("Works!",commandData.msg);
