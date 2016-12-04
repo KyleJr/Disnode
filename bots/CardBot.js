a2512e84f345654833ecd9758530dced3ff77dcd
AtecStudios <VictoryForPhil@gmail.com>
2016-12-03 22:39:18 -0800

    Clean up

--- bots/CardBot.js
+++ bots/CardBot.js
@@ -1,6 +1,6 @@
 var DisnodeBot = require("../src/Disnode.js"); //defines DisnodeBot
 // above is testing require use require("disnode"); instead if you installed via NPM
-var testBot = new DisnodeBot("./Bots/TestBotConfig.json"); //Defines the testBot in the "" is where your discord bot oauth token would go
+var testBot = new DisnodeBot("./Bots/CADConfig.json"); //Defines the testBot in the "" is where your discord bot oauth token would go
 
 var OnLoad = function(){
   testBot.startBot();
